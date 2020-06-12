4ffb6f73f96fda58723cb9ceea115710800bc0db
Brian Bagrov <Brianbagrov@gmail.com>
2020-06-11 20:40:11 -0400

    Done. >:)

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,7 +3,15 @@
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
-axios.get("ttps://api.github.com/stratified");
+
+axios
+  .get("https://api.github.com/users/stratified")
+  .then((res) => {
+    gitHubUser(res);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
@@ -17,7 +25,7 @@ axios.get("ttps://api.github.com/stratified");
   STEP 4: Pass the data received from Github into your function,
     and append the returned markup to the DOM as a child of .cards
 */
-
+const cards = document.querySelector(".cards");
 /*
   STEP 5: Now that you have your own card getting added to the DOM, either
     follow this link in your browser https://api.github.com/users/<Your github name>/followers,
@@ -29,7 +37,25 @@ axios.get("ttps://api.github.com/stratified");
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const friendsArray = [
+  "Johnjohnn",
+  "dterran2",
+  "aperez9423",
+  "melanie-mendoza",
+  "chqui6",
+];
+
+friendsArray.forEach((friend) => {
+  axios
+    .get(`https://api.github.com/users/${friend}`)
+    .then((res) => {
+      cards.append(gitHubUser(res));
+      console.log(res);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
@@ -51,11 +77,57 @@ const followersArray = [];
     </div>
 */
 
-/*
-  List of LS Instructors Github username's:
-    tetondan
-    dustinmyers
-    justsml
-    luishrd
-    bigknell
-*/
+function gitHubUser(user) {
+  const card = document.createElement("div");
+  const userImg = document.createElement("img");
+  const cardInfo = document.createElement("div");
+  const name = document.createElement("h3");
+  const username = document.createElement("p");
+  const location = document.createElement("p");
+  const profile = document.createElement("p");
+  const link = document.createElement("a");
+  const followers = document.createElement("p");
+  const following = document.createElement("p");
+  const bio = document.createElement("p");
+
+  name.textContent = user.data.name;
+  username.textContent = user.data.login;
+  location.textContent = `Location: ${user.data.location}`;
+  profile.textContent = `Profile: `;
+  link.href = user.data.html_url;
+  link.textContent = user.data.html_url;
+  followers.textContent = `Followers: ${user.data.followers}`;
+  following.textContent = `Following: ${user.data.following}`;
+  bio.textContent = `Bio: ${user.data.bio}`;
+  userImg.src = user.data.avatar_url;
+
+  card.classList.add("card");
+  cardInfo.classList.add("card-info");
+  name.classList.add("name");
+  username.classList.add("username");
+
+  card.appendChild(userImg);
+  card.appendChild(cardInfo);
+  cardInfo.appendChild(name);
+  cardInfo.appendChild(username);
+  cardInfo.appendChild(location);
+  cardInfo.appendChild(profile);
+  profile.appendChild(link);
+  cardInfo.appendChild(followers);
+  cardInfo.appendChild(following);
+  cardInfo.appendChild(bio);
+
+  cards.prepend(card);
+
+  if (user.data.location === null) {
+    location.textContent = "Location: Unknown.";
+  }
+  if (user.data.bio === null) {
+    bio.textContent = "Bio: Unknown.";
+  }
+  if (user.data.name === null) {
+    name.textContent = user.data.login;
+  }
+
+  return card;
+}
